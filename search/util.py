acee724a290509ee8df8482e6a70d8876907b7e6
sbogacz <sbogacz@zvelo.com>
2017-06-25 16:17:25 -0600

    fix some comment formatting

--- search/util.py
+++ search/util.py
@@ -84,11 +84,11 @@ class PriorityQueue:
 
 class PriorityQueueWithFunction(PriorityQueue):
     """
-  Implements a priority queue with the same push/pop signature of the
-  Queue and the Stack classes. This is designed for drop-in replacement for
-  those two classes. The caller has to provide a priority function, which
-  extracts each item's priority.
-  """
+    Implements a priority queue with the same push/pop signature of the
+    Queue and the Stack classes. This is designed for drop-in replacement for
+    those two classes. The caller has to provide a priority function, which
+    extracts each item's priority.
+    """
 
     def __init__(self, priorityFunction):
         "priorityFunction (item) -> priority"
