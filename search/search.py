fd7df120eb6c435c72ff068747436e98a4e6afee
sbogacz <sbogacz@zvelo.com>
2017-06-25 15:33:30 -0600

    Implement the search algorithms for graph BFS and DFS

--- search/search.py
+++ search/search.py
@@ -6,62 +6,65 @@
 # John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
 """
-In search.py, you will implement generic search algorithms which are called 
+In search.py, you will implement generic search algorithms which are called
 by Pacman agents (in searchAgents.py).
 """
 
 import util
+import pacman
 
 
 class SearchProblem:
     """
-  This class outlines the structure of a search problem, but doesn't implement
-  any of the methods (in object-oriented terminology: an abstract class).
-  
-  You do not need to change anything in this class, ever.
-  """
+    This class outlines the structure of a search problem, but doesn't
+    implement any of the methods (in object-oriented terminology: an
+    abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
 
     def getStartState(self):
         """
-     Returns the start state for the search problem 
-     """
+        Returns the start state for the search problem
+        """
         util.raiseNotDefined()
 
     def isGoalState(self, state):
         """
-       state: Search state
-    
-     Returns True if and only if the state is a valid goal state
-     """
+        state: Search state
+
+        Returns True if and only if the state is a valid goal state
+        """
         util.raiseNotDefined()
 
     def getSuccessors(self, state):
         """
-       state: Search state
-     
-     For a given state, this should return a list of triples, 
-     (successor, action, stepCost), where 'successor' is a 
-     successor to the current state, 'action' is the action
-     required to get there, and 'stepCost' is the incremental 
-     cost of expanding to that successor
-     """
+        state: Search state
+
+        For a given state, this should return a list of triples,
+        (successor, action, stepCost), where 'successor' is a
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental
+        cost of expanding to that successor
+        """
         util.raiseNotDefined()
 
     def getCostOfActions(self, actions):
         """
-      actions: A list of actions to take
- 
-     This method returns the total cost of a particular sequence of actions.  The sequence must
-     be composed of legal moves
-     """
+        actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves
+        """
         util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
     """
-  Returns a sequence of moves that solves tinyMaze.  For any other
-  maze, the sequence of moves will be incorrect, so only use this for tinyMaze
-  """
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for
+    tinyMaze
+    """
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
@@ -74,7 +77,7 @@ def depthFirstSearch(problem):
     [2nd Edition: p 75, 3rd Edition: p 87]
 
     Your search algorithm needs to return a list of actions that reaches
-    the goal.  Make sure to implement a graph search algorithm 
+    the goal.  Make sure to implement a graph search algorithm
     [2nd Edition: Fig. 3.18, 3rd Edition: Fig 3.7].
 
     To get started, you might want to try some of these simple commands to
@@ -85,7 +88,24 @@ def depthFirstSearch(problem):
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-    #if problem.isGoalState(problem.getStartState()):
+    return searchFrontier(problem, util.Stack())
+            
+def searchFrontier(problem, frontier):
+    # if we're already at the goal, then we're done
+    if problem.isGoalState(problem.getStartState()):
+        return []
+    explored = set()
+    frontier.push((problem.getStartState(), [], 0)) # state, actions to get there, cost
+    while not frontier.isEmpty():
+        state, path, costSoFar  = frontier.pop()
+        explored.add(state)
+        if problem.isGoalState(state):
+            return path
+        for successor, action, cost in problem.getSuccessors(state):
+            if successor in explored:
+                continue
+            frontier.push((successor, path + [action], costSoFar + cost))
+    return []
 
 
 def breadthFirstSearch(problem):
@@ -94,7 +114,7 @@ def breadthFirstSearch(problem):
   [2nd Edition: p 73, 3rd Edition: p 82]
   """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    return searchFrontier(problem, util.Queue())
 
 
 def uniformCostSearch(problem):
